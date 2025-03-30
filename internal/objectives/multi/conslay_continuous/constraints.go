f5c6d92d467436910fa51281b1818461e08d9dd2
Dat Dao Vu <dat.daovu@gmail.com>
2025-03-30 16:29:00 +0700

    add feasible and infeasible results test for Constraints

--- internal/objectives/multi/conslay_continuous/constraints.go
+++ internal/objectives/multi/conslay_continuous/constraints.go
@@ -1,7 +1,6 @@
 package conslay_continuous
 
 import (
-	"maps"
 	"math"
 	"slices"
 )
@@ -10,21 +9,46 @@ type Constrainter interface {
 	Eval(map[string]Location) float64
 }
 
+// Cover Range of Crane
+
 type CoverRangeCraneConstraint struct {
 	Cranes []Crane
+	Phases [][]string
 }
 
 func (c CoverRangeCraneConstraint) Eval(mapLocations map[string]Location) float64 {
 	amount := 0.0
-	for i := 0; i < len(c.Cranes); i++ {
-		buildings := make([]Location, len(c.Cranes[i].BuildingName))
-		for j := 0; j < len(c.Cranes[i].BuildingName); j++ {
-			buildings[j] = mapLocations[c.Cranes[i].BuildingName[j]]
-		}
 
-		_, val := IsCoverRangeOfCrane(c.Cranes[i], buildings)
-		amount += val
+	for _, phase := range c.Phases {
+		for _, crane := range c.Cranes {
+			// check crane exists in phase
+			if !slices.Contains(phase, crane.Symbol) {
+				continue
+			}
+
+			buildings := make([]Location, 0)
+			for _, building := range crane.BuildingName {
+				if !slices.Contains(phase, building) {
+					continue
+				}
+
+				buildingLocation := mapLocations[building]
+				buildings = append(buildings, buildingLocation)
+			}
+			_, val := IsCoverRangeOfCrane(crane, buildings)
+			amount += val
+		}
 	}
+
+	//for i := 0; i < len(c.Cranes); i++ {
+	//	buildings := make([]Location, len(c.Cranes[i].BuildingName))
+	//	for j := 0; j < len(c.Cranes[i].BuildingName); j++ {
+	//		buildings[j] = mapLocations[c.Cranes[i].BuildingName[j]]
+	//	}
+	//
+	//	_, val := IsCoverRangeOfCrane(c.Cranes[i], buildings)
+	//	amount += val
+	//}
 
 	return amount
 }
@@ -77,19 +101,32 @@ func IsCoverRangeOfCrane(crane Crane, buildings []Location) (bool, float64) {
 
 }
 
+// Overlap Constraint
+
 type OverlapConstraint struct {
+	Phases [][]string
 }
 
 func (c OverlapConstraint) Eval(mapLocations map[string]Location) float64 {
 	amount := 0.0
 
-	// convert map to slice
-	sliceLocations := slices.Collect(maps.Values(mapLocations))
+	//// convert map to slice
+	//sliceLocations := slices.Collect(maps.Values(mapLocations))
+	//
+	//for i := 0; i < len(sliceLocations)-1; i++ {
+	//	for j := i + 1; j < len(sliceLocations); j++ {
+	//		_, val := IsOverlapped(sliceLocations[i], sliceLocations[j])
+	//		amount += val
+	//	}
+	//}
 
-	for i := 0; i < len(sliceLocations)-1; i++ {
-		for j := i + 1; j < len(sliceLocations); j++ {
-			_, val := IsOverlapped(sliceLocations[i], sliceLocations[j])
-			amount += val
+	for _, phase := range c.Phases {
+		numberOfLocations := len(phase)
+		for i := 0; i < numberOfLocations-1; i++ {
+			for j := i + 1; j < numberOfLocations; j++ {
+				_, val := IsOverlapped(mapLocations[phase[i]], mapLocations[phase[j]])
+				amount += val
+			}
 		}
 	}
 
@@ -112,15 +149,19 @@ func IsOverlapped(b1, b2 Location) (bool, float64) {
 	return true, math.Max(0, l1) + math.Max(0, l2)
 }
 
+// Out of Bounds Constraint
+
 type OutOfBoundsConstraint struct {
 	MinWidth  float64
 	MaxWidth  float64
 	MinLength float64
 	MaxLength float64
+	Phases    [][]string // Out of bounds the construction layout => No need to calculate each phase
 }
 
 func (c OutOfBoundsConstraint) Eval(mapLocations map[string]Location) float64 {
 	amount := 0.0
+
 	for _, v := range mapLocations {
 		if !v.IsFixed {
 			_, val := IsOutOfBound(c.MinLength, c.MaxLength, c.MinWidth, c.MaxWidth, v)
@@ -142,4 +183,33 @@ func IsOutOfBound(minL, maxL, minW, maxW float64, b Location) (bool, float64) {
 	}
 
 	return true, math.Max(0, l1) + math.Max(0, l2) + math.Max(0, l3) + math.Max(0, l4)
+}
+
+// Inclusive Zone
+
+type Zone struct {
+	Location
+	BuildingNames []string
+	Size          float64
+}
+
+type InclusiveZoneConstraint struct {
+	Zones  []Zone
+	Phases [][]string // Building is fixed => No need to calculate each phase
+}
+
+func (c InclusiveZoneConstraint) Eval(mapLocations map[string]Location) float64 {
+	amount := 0.0
+	for _, zone := range c.Zones {
+		minL := zone.Coordinate.X - zone.Length/2 - zone.Size
+		maxL := zone.Coordinate.X + zone.Length/2 + zone.Size
+		minW := zone.Coordinate.Y - zone.Width/2 - zone.Size
+		maxW := zone.Coordinate.Y + zone.Width/2 + zone.Size
+
+		for _, building := range zone.BuildingNames {
+			_, val := IsOutOfBound(minL, maxL, minW, maxW, mapLocations[building])
+			amount += val
+		}
+	}
+	return amount
 }
--- internal/objectives/multi/conslay_continuous/constraints_test.go
+++ internal/objectives/multi/conslay_continuous/constraints_test.go
@@ -1,6 +1,7 @@
 package conslay_continuous
 
 import (
+	"golang-moaha-construction/internal/util"
 	"math"
 	"testing"
 )
@@ -513,23 +514,486 @@ func TestIsOutOfBoundFalse(t *testing.T) {
 	}
 }
 
-func TestOutOfBoundsConstraint_Eval(t *testing.T) {
+func CreateInputLocation(feasible bool) map[string]Location {
+	//input := []float64{
+	//	41.8984994960664, 16.0789402087708, 0.110667114989373,
+	//	40.3018087791441, 6.54792941746860, 0.632349545372518,
+	//	43.0538143534362, 81.9094405733262, 0.0819942691480144,
+	//	49.7610024122533, 57.2431089610756, 0.845980667765488,
+	//	81.3261517318950, 41.8071744013404, 0.274381079260441,
+	//	101.490485902993, 11.1774705429599, 0.566086701250327,
+	//	43.6721024451348, 9.42961655802787, 0.875812362483772,
+	//	48.5660821450197, 54.0455646998945, 0.207017324531373,
+	//	76.4180335261621, 41.8787903034157, 0.298433966901801,
+	//	108.028665273018, 36.9845037867221, 0.712680757776487,
+	//	63.8375790712078, 91.2094786904858, 0.746326249229960,
+	//	110.579901257085, 14.7798350113455, 0.122726427684860,
+	//} // feasible
+
+	//input := []float64{
+	//	60.2117021287296, 70.6492302951395, 0.840088279889616,
+	//	70.8063495854695, 20.8484429816236, 0.930459707285301,
+	//	16.0620484334653, 11.2728104676465, 0.256779152503716,
+	//	66.8271432818314, 79.6322545348163, 0.994330695133932,
+	//	97.4966707756999, 13.6022897445312, 0.142998150825580,
+	//	1.51514052087480, 80.0719162226562, 0.0752335982572160,
+	//	36.7111938858722, 11.9943304146193, 0.608734625270310,
+	//	48.6579882916605, 61.3097045687976, 0.0711765983287040,
+	//	7.04418109569817, 87.8287852386491, 0.736732593589075,
+	//	42.2938936308434, 65.2135456501644, 0.156252552003082,
+	//	21.9910306881593, 85.0044587159497, 0.464587709048142,
+	//	23.2731387385839, 90.0053928795082, 0.790347360934255,
+	//} // infeasible
+
+	locations := make(map[string]Location)
+	if feasible {
+		locations = map[string]Location{
+			"TF1": Location{
+				Coordinate: Coordinate{X: 41.8984994960664, Y: 16.0789402087708},
+				Rotation:   false,
+				Length:     12,
+				Width:      5,
+				IsFixed:    false,
+				Symbol:     "TF1",
+				Name:       "Pile storage yard #1",
+			},
+			"TF2": Location{
+				Coordinate: Coordinate{X: 40.3018087791441, Y: 6.54792941746860},
+				Rotation:   true,
+				Length:     5,
+				Width:      12,
+				IsFixed:    false,
+				Symbol:     "TF2",
+				Name:       "Pile storage yard #2",
+			},
+			"TF3": Location{
+				Coordinate: Coordinate{X: 43.0538143534362, Y: 81.9094405733262},
+				Rotation:   false,
+				Length:     8,
+				Width:      14,
+				IsFixed:    false,
+				Symbol:     "TF3",
+				Name:       "Site office",
+			},
+			"TF4": Location{
+				Coordinate: Coordinate{X: 49.7610024122533, Y: 57.2431089610756},
+				Rotation:   true,
+				Length:     7,
+				Width:      14,
+				IsFixed:    false,
+				Symbol:     "TF4",
+				Name:       "Rebar process yard",
+			},
+			"TF5": Location{
+				Coordinate: Coordinate{X: 81.3261517318950, Y: 41.8071744013404},
+				Rotation:   false,
+				Length:     14,
+				Width:      7,
+				IsFixed:    false,
+				Symbol:     "TF5",
+				Name:       "Formwork process yard",
+			},
+			"TF6": Location{
+				Coordinate: Coordinate{X: 101.490485902993, Y: 11.1774705429599},
+				Rotation:   true,
+				Length:     4,
+				Width:      4,
+				IsFixed:    false,
+				Symbol:     "TF6",
+				Name:       "Electrician hut",
+			},
+			"TF7": Location{
+				Coordinate: Coordinate{X: 43.6721024451348, Y: 9.42961655802787},
+				Rotation:   true,
+				Length:     12,
+				Width:      10,
+				IsFixed:    false,
+				Symbol:     "TF7",
+				Name:       "Ready-mix concrete area",
+			},
+			"TF8": Location{
+				Coordinate: Coordinate{X: 48.5660821450197, Y: 54.0455646998945},
+				Rotation:   false,
+				Length:     12,
+				Width:      6,
+				IsFixed:    false,
+				Symbol:     "TF8",
+				Name:       "Prefabricated component yard #1 (slab, staircase)",
+			},
+			"TF9": Location{
+				Coordinate: Coordinate{X: 76.4180335261621, Y: 41.8787903034157},
+				Rotation:   false,
+				Length:     12,
+				Width:      6,
+				IsFixed:    false,
+				Symbol:     "TF9",
+				Name:       "Prefabricated component yard #2 (beam, column)",
+			},
+			"TF10": Location{
+				Coordinate: Coordinate{X: 108.028665273018, Y: 36.9845037867221},
+				Rotation:   true,
+				Length:     6,
+				Width:      10,
+				IsFixed:    false,
+				Symbol:     "TF10",
+				Name:       "Prefabricated component yard #3 (external wall)",
+			},
+			"TF11": Location{
+				Coordinate: Coordinate{X: 63.8375790712078, Y: 91.2094786904858},
+				Rotation:   true,
+				Length:     4,
+				Width:      6,
+				IsFixed:    false,
+				Symbol:     "TF11",
+				Name:       "Dangerous goods warehouse",
+			},
+			"TF12": Location{
+				Coordinate: Coordinate{X: 110.579901257085, Y: 14.7798350113455},
+				Rotation:   false,
+				Length:     8,
+				Width:      6,
+				IsFixed:    false,
+				Symbol:     "TF12",
+				Name:       "ME storage warehouse",
+			},
+			"TF13": Location{
+				Coordinate: Coordinate{X: 72, Y: 22},
+				Rotation:   false,
+				Length:     52,
+				Width:      24,
+				IsFixed:    true,
+				Symbol:     "TF13",
+				Name:       "Building",
+			},
+			"TF14": {
+				Coordinate: Coordinate{X: 72, Y: 36},
+				Rotation:   false,
+				Length:     2,
+				Width:      2,
+				IsFixed:    true,
+				Symbol:     "TF14",
+				Name:       "Tower crane",
+			},
+			"TF15": {
+				Coordinate: Coordinate{X: 43, Y: 22},
+				Rotation:   false,
+				Length:     3,
+				Width:      3,
+				IsFixed:    true,
+				Symbol:     "TF15",
+				Name:       "Hoist",
+			},
+			"TF16": {
+				Coordinate: Coordinate{X: 5, Y: 92},
+				Rotation:   false,
+				Length:     3,
+				Width:      4,
+				IsFixed:    false,
+				Symbol:     "TF16",
+				Name:       "Security room",
+			},
+		}
+	} else {
+		locations = map[string]Location{
+			"TF1": Location{
+				Coordinate: Coordinate{X: 60.2117021287296, Y: 70.6492302951395},
+				Rotation:   true,
+				Length:     5,
+				Width:      12,
+				IsFixed:    false,
+				Symbol:     "TF1",
+				Name:       "Pile storage yard #1",
+			},
+			"TF2": Location{
+				Coordinate: Coordinate{X: 70.8063495854695, Y: 20.8484429816236},
+				Rotation:   true,
+				Length:     5,
+				Width:      12,
+				IsFixed:    false,
+				Symbol:     "TF2",
+				Name:       "Pile storage yard #2",
+			},
+			"TF3": Location{
+				Coordinate: Coordinate{X: 16.0620484334653, Y: 11.2728104676465},
+				Rotation:   false,
+				Length:     8,
+				Width:      14,
+				IsFixed:    false,
+				Symbol:     "TF3",
+				Name:       "Site office",
+			},
+			"TF4": Location{
+				Coordinate: Coordinate{X: 66.8271432818314, Y: 79.6322545348163},
+				Rotation:   true,
+				Length:     7,
+				Width:      14,
+				IsFixed:    false,
+				Symbol:     "TF4",
+				Name:       "Rebar process yard",
+			},
+			"TF5": Location{
+				Coordinate: Coordinate{X: 97.4966707756999, Y: 13.6022897445312},
+				Rotation:   false,
+				Length:     14,
+				Width:      7,
+				IsFixed:    false,
+				Symbol:     "TF5",
+				Name:       "Formwork process yard",
+			},
+			"TF6": Location{
+				Coordinate: Coordinate{X: 1.51514052087480, Y: 80.0719162226562},
+				Rotation:   false,
+				Length:     4,
+				Width:      4,
+				IsFixed:    false,
+				Symbol:     "TF6",
+				Name:       "Electrician hut",
+			},
+			"TF7": Location{
+				Coordinate: Coordinate{X: 36.7111938858722, Y: 11.9943304146193},
+				Rotation:   true,
+				Length:     12,
+				Width:      10,
+				IsFixed:    false,
+				Symbol:     "TF7",
+				Name:       "Ready-mix concrete area",
+			},
+			"TF8": Location{
+				Coordinate: Coordinate{X: 48.6579882916605, Y: 61.3097045687976},
+				Rotation:   false,
+				Length:     12,
+				Width:      6,
+				IsFixed:    false,
+				Symbol:     "TF8",
+				Name:       "Prefabricated component yard #1 (slab, staircase)",
+			},
+			"TF9": Location{
+				Coordinate: Coordinate{X: 7.04418109569817, Y: 87.8287852386491},
+				Rotation:   true,
+				Length:     6,
+				Width:      12,
+				IsFixed:    false,
+				Symbol:     "TF9",
+				Name:       "Prefabricated component yard #2 (beam, column)",
+			},
+			"TF10": Location{
+				Coordinate: Coordinate{X: 42.2938936308434, Y: 65.2135456501644},
+				Rotation:   false,
+				Length:     10,
+				Width:      6,
+				IsFixed:    false,
+				Symbol:     "TF10",
+				Name:       "Prefabricated component yard #3 (external wall)",
+			},
+			"TF11": Location{
+				Coordinate: Coordinate{X: 21.9910306881593, Y: 85.0044587159497},
+				Rotation:   false,
+				Length:     6,
+				Width:      4,
+				IsFixed:    false,
+				Symbol:     "TF11",
+				Name:       "Dangerous goods warehouse",
+			},
+			"TF12": Location{
+				Coordinate: Coordinate{X: 23.2731387385839, Y: 90.0053928795082},
+				Rotation:   true,
+				Length:     6,
+				Width:      8,
+				IsFixed:    false,
+				Symbol:     "TF12",
+				Name:       "ME storage warehouse",
+			},
+			"TF13": Location{
+				Coordinate: Coordinate{X: 72, Y: 22},
+				Rotation:   false,
+				Length:     52,
+				Width:      24,
+				IsFixed:    true,
+				Symbol:     "TF13",
+				Name:       "Building",
+			},
+			"TF14": {
+				Coordinate: Coordinate{X: 72, Y: 36},
+				Rotation:   false,
+				Length:     2,
+				Width:      2,
+				IsFixed:    true,
+				Symbol:     "TF14",
+				Name:       "Tower crane",
+			},
+			"TF15": {
+				Coordinate: Coordinate{X: 43, Y: 22},
+				Rotation:   false,
+				Length:     3,
+				Width:      3,
+				IsFixed:    true,
+				Symbol:     "TF15",
+				Name:       "Hoist",
+			},
+			"TF16": {
+				Coordinate: Coordinate{X: 5, Y: 92},
+				Rotation:   false,
+				Length:     3,
+				Width:      4,
+				IsFixed:    false,
+				Symbol:     "TF16",
+				Name:       "Security room",
+			},
+		}
+	}
+
+	return locations
+}
+
+func CreateInputPhases() [][]string {
+	return [][]string{
+		{"TF1", "TF2", "TF3", "TF6", "TF13", "TF16"},
+		{"TF3", "TF4", "TF5", "TF6", "TF7", "TF13", "TF16"},
+		{"TF3", "TF4", "TF5", "TF6", "TF7", "TF11", "TF13", "TF14", "TF15", "TF16"},
+		{"TF3", "TF6", "TF8", "TF9", "TF11", "TF13", "TF14", "TF15", "TF16"},
+		{"TF3", "TF6", "TF10", "TF11", "TF12", "TF13", "TF14", "TF15", "TF16"},
+	}
+}
+
+func TestOutOfBoundsConstraint_Eval_Feasible(t *testing.T) {
 	// Setup
 	outOfBoundsConstraint := OutOfBoundsConstraint{
 		MaxWidth:  95,
 		MaxLength: 120,
+		Phases:    CreateInputPhases(),
+	}
+
+	locations := CreateInputLocation(true)
+
+	penalty := outOfBoundsConstraint.Eval(locations)
+	if math.Round(penalty) != 0 {
+		t.Errorf("expected penalty to be 0, got %f", penalty)
 	}
 
 }
 
-func TestOverlapConstraint_Eval(t *testing.T) {
-	overlapConstraint := OverlapConstraint{}
+func TestOverlapConstraint_Eval_Feasible(t *testing.T) {
+	overlapConstraint := OverlapConstraint{
+		Phases: CreateInputPhases(),
+	}
+
+	locations := CreateInputLocation(true)
+
+	penalty := overlapConstraint.Eval(locations)
+	if util.RoundTo(penalty, 2) != 26.68 {
+		t.Errorf("expected penalty to be 26.68, got %f", penalty)
+	}
 }
 
-func TestCoverRangeCraneConstraint_Eval(t *testing.T) {
+func TestCoverRangeCraneConstraint_Eval_Feasible(t *testing.T) {
+	locations := CreateInputLocation(true)
 	coverRangeConstraint := CoverRangeCraneConstraint{
 		Cranes: []Crane{
-			{Location: , BuildingName: , Radius: },
+			{
+				Location:     locations["TF14"],
+				BuildingName: []string{"TF4", "TF5", "TF8", "TF9", "TF10"},
+				Radius:       40,
+			},
 		},
+	}
+
+	penalty := coverRangeConstraint.Eval(locations)
+	if math.Round(penalty) != 0 {
+		t.Errorf("expected penalty to be 0, got %f", penalty)
+	}
+}
+
+func TestInclusiveZoneConstraint_Eval_Feasible(t *testing.T) {
+	locations := CreateInputLocation(true)
+	zoneConstraint := InclusiveZoneConstraint{
+		Zones: []Zone{
+			{
+				Location:      locations["TF13"],
+				BuildingNames: []string{"TF7"},
+				Size:          20,
+			},
+			{
+				Location:      locations["TF13"],
+				BuildingNames: []string{"TF1", "TF2"},
+				Size:          15,
+			},
+		},
+	}
+
+	penalty := zoneConstraint.Eval(locations)
+	if math.Round(penalty) != 0 {
+		t.Errorf("expected penalty to be 0, got %f", penalty)
+	}
+}
+
+func TestOutOfBoundsConstraint_Eval_Infeasible(t *testing.T) {
+	// Setup
+	outOfBoundsConstraint := OutOfBoundsConstraint{
+		MaxWidth:  95,
+		MaxLength: 120,
+		Phases:    CreateInputPhases(),
+	}
+
+	locations := CreateInputLocation(false)
+
+	penalty := outOfBoundsConstraint.Eval(locations)
+	if util.RoundTo(penalty, 2) != 0.48 {
+		t.Errorf("expected penalty to be 0.48, got %f", penalty)
+	}
+
+}
+
+func TestOverlapConstraint_Eval_Infeasible(t *testing.T) {
+	overlapConstraint := OverlapConstraint{
+		Phases: CreateInputPhases(),
+	}
+
+	locations := CreateInputLocation(false)
+
+	penalty := overlapConstraint.Eval(locations)
+	if util.RoundTo(penalty, 2) != 85.37 {
+		t.Errorf("expected penalty to be 85.37, got %f", penalty)
+	}
+}
+
+func TestCoverRangeCraneConstraint_Eval_Infeasible(t *testing.T) {
+	locations := CreateInputLocation(false)
+	coverRangeConstraint := CoverRangeCraneConstraint{
+		Cranes: []Crane{
+			{
+				Location:     locations["TF14"],
+				BuildingName: []string{"TF4", "TF5", "TF8", "TF9", "TF10"},
+				Radius:       40,
+			},
+		},
+		Phases: CreateInputPhases(),
+	}
+
+	penalty := coverRangeConstraint.Eval(locations)
+	if util.RoundTo(penalty, 2) != 208.81 {
+		t.Errorf("expected penalty to be 208.81, got %f", penalty)
+	}
+}
+
+func TestInclusiveZoneConstraint_Eval_Infeasible(t *testing.T) {
+	locations := CreateInputLocation(false)
+	zoneConstraint := InclusiveZoneConstraint{
+		Zones: []Zone{
+			{
+				Location:      locations["TF13"],
+				BuildingNames: []string{"TF7"},
+				Size:          20,
+			},
+			{
+				Location:      locations["TF13"],
+				BuildingNames: []string{"TF1", "TF2"},
+				Size:          15,
+			},
+		},
+	}
+
+	penalty := zoneConstraint.Eval(locations)
+	if util.RoundTo(penalty, 2) != 27.65 {
+		t.Errorf("expected penalty to be 27.65, got %f", penalty)
 	}
 }
